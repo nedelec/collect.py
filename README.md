7ffce09b2d1ab07250a52555b03d61bbfe733c12
nedelec <nedelec@embl.de>
2017-10-02 10:22:37 +0200

    collect.py accepts '%%%%'

--- README.md
+++ README.md
@@ -1,30 +1,46 @@
 # collect.py
-Rename or Copy specified files or directories
+Rename or copy specified files or directories
 
-Synopsis:
+###Synopsis:
     
-    Rename files or folders using a pattern containing an index as in 'image0001.png'.
-    This number in the file name will be incremented automatically for each file,
-    and no existing file will not overwritten, such that this can be used to pool
-    together many similar files in a common directory.
+    Rename files or folders following a pattern containing an integer index,
+    as in 'image0001.png'. The file will be moved in the current directory
     
-Syntax:
+    The number in the file name is incremented automatically for each file, and
+    also if files with this name already exist. Thus pre-existing files are not 
+    overwritten, such that 'collect.py' can be used to pool together many similar
+    files in a common directory.
     
-    collect.py PATTERN [INTEGER] [--copy] PATHS
+###Syntax:
+    
+    collect.py PATTERN [INTEGER] [--copy] PATH1 [PATH2] [PATH3] ...
 
-Arguments:
+###Arguments:
     
-    PATTERN is a string containing '%i' or '%0Xi', for example 'image%04i.png'
+    PATTERN specifies the name of the output files, and should contain a variable
+    part that will be replaced by an integer. It can be a 'scanf' compatible 
+    pattern such as '%i' or '%0Xi', for example 'image%04i.png'.
+    A character '%' repeated multiple times, such as `%%%%` or `%%%%%%`, can be 
+    used to directly specify the size of the integer portion of the name.
+    
+    The pattern can include a directory, and if this directory does not exist,
+    collect.py will attemtps to create it before moving the file.
+
     if specified, --copy will copy the files/directory instead of moving them
-    if specified, INTEGER is the first index to be used (default=0)
-    PATHS is a list of file or directories
-
-Examples:
     
-    collect.py image%04i.png  *.png
+    if specified, INTEGER is the first index to be used (default=0)
+
+    PATH1, PATH2, etc. is a list of files or directories
+
+###Examples:
+    
+    collect.py image%%%%.png  *.png
        will rename image files to: image0000.png, image0001.png, etc.
     
-    collect.py --copy image%04i.png 1 run*/image.png"
+    collect.py --copy image%%%%.png 1 run*/image.png
        will copy the image files, starting at index 1
+
+    collect.py run%%%%%/config.cym config*.cym
+       will create directories run???? and copy the `config*.cym` files into them
     
-F. Nedelec, 2012--2017.
+F. Nedelec, 2012--2017. Last modified 2.10.2017
--- collect.py
+++ collect.py
@@ -8,36 +8,47 @@
 """
 Synopsis:
     
-    Rename files or folders using a pattern containing an index as in 'image0001.png'.
-    This number in the file name will be incremented automatically for each file,
-    and no existing file will not overwritten, such that this can be used to pool
-    together many similar files in a common directory.
+    Rename files or folders following a pattern containing an integer index,
+    as in 'image0001.png'. The file will be moved in the current directory
+    
+    The number in the file name is incremented automatically for each file, and
+    also if files with this name already exist. Thus pre-existing files are not 
+    overwritten, such that 'collect.py' can be used to pool together many similar
+    files in a common directory.
     
 Syntax:
     
-    collect.py PATTERN [INTEGER] [--copy] PATHS
+    collect.py PATTERN [INTEGER] [--copy] PATH1 [PATH2] [PATH3] ...
 
 Arguments:
     
-    PATTERN is a string containing '%i' or '%0Xi', for example 'image%04i.png'
-    if specified, --copy will copy the files/directory instead of moving them
-    if specified, INTEGER is the first index to be used (default=0)
-    If the patterns includes a directory, collect.py will attemtps to create it.
+    PATTERN specifies the name of the output files, and should contain a variable
+    part that will be replaced by an integer. It can be a 'scanf' compatible 
+    pattern such as '%i' or '%0Xi', for example 'image%04i.png'.
+    A character '%' repeated multiple times, such as `%%%%` or `%%%%%%`, can be 
+    used to directly specify the size of the integer portion of the name.
+    
+    The pattern can include a directory, and if this directory does not exist,
+    collect.py will attemtps to create it before moving the file.
 
-    PATHS is a list of file or directories
+    if specified, --copy will copy the files/directory instead of moving them
+    
+    if specified, INTEGER is the first index to be used (default=0)
+
+    PATH1, PATH2, etc. is a list of files or directories
 
 Examples:
     
-    collect.py image%04i.png  *.png
+    collect.py image%%%%.png  *.png
        will rename image files to: image0000.png, image0001.png, etc.
     
-    collect.py --copy image%04i.png 1 run*/image.png
+    collect.py --copy image%%%%.png 1 run*/image.png
        will copy the image files, starting at index 1
 
-    collect.py run%04i/config.cym config*.cym
-       will create directories run???? and copy the config*.cym files to them
+    collect.py run%%%%%/config.cym config*.cym
+       will create directories run???? and copy the `config*.cym` files into them
     
-F. Nedelec, 2012--2017. Last modified 1.09.2017
+F. Nedelec, 2012--2017. Last modified 2.10.2017
 """
 
 
@@ -76,16 +87,25 @@ def main(args):
     if os.path.isfile(pattern):
         sys.stderr.write("Error: first argument should be the pattern used to build output file name")
         return 1
-    pattern % 0
     try:
-        str = ( pattern % 0 )
+        res = ( pattern % 0 )
     except:
-        sys.stderr.write("Error: the pattern should accept an integer: eg. '%i' or '%04i'\n")
-        return 1
-    for c in str:
+        # check for repeated '%' character:
+        for n in range(10,0,-1):
+            s = pattern.find('%'*n)
+            if s > 0:
+                pattern = pattern.replace('%'*n, '%0'+str(n)+'i', 1);
+                break
+        try:
+            res = ( pattern % 0 )
+        except:
+            sys.stderr.write("Error: the pattern should accept an integer: eg. '%04i'\n")
+            return 1
+    for c in res:
         if curses.ascii.isspace(c):
             sys.stderr.write("Error: the pattern includes or generates white space character\n")
             return 1
+    # go
     paths = []
     idx = 0
     # parse arguments:
@@ -127,7 +147,7 @@ def main(args):
 #------------------------------------------------------------------------
 
 if __name__ == "__main__":
-    if len(sys.argv) < 2 or sys.argv[1]=='help':
+    if len(sys.argv) < 2 or sys.argv[1]=='help' or sys.argv[1]=='--help':
         print(__doc__)
     else:
         main(sys.argv[1:])
